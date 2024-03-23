9af7ad1adda3011b0a99b821124f87f897abcb6c
Dmitry Nikolayev <reacttsxgrammer@gmail.com>
2024-03-23 10:45:08 -0500

    fix: fix document

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -134,8 +134,14 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  const handledPromises = promises.map((promise) =>
+    promise.then(
+      (result) => result,
+      () => null
+    )
+  );
+  return Promise.all(handledPromises);
 }
 
 /**
