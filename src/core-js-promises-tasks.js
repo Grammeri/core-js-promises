a575fe7c660ec2cd6e43ca49c4f77feb17a96c43
Dmitry Nikolayev <reacttsxgrammer@gmail.com>
2024-03-23 10:15:36 -0500

    feat: resolve queuPromises

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -156,8 +156,19 @@ function getAllResult(/* promises */) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  let resultString = '';
+  let chain = Promise.resolve();
+
+  promises.forEach((promise) => {
+    chain = chain
+      .then(() => promise)
+      .then((result) => {
+        resultString += result.toString();
+      });
+  });
+
+  return chain.then(() => resultString);
 }
 
 module.exports = {
